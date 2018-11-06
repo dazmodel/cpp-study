4766c2788a59cc5bd1dd26423b742425d599e5f4
Sergii Sinienok <sergii.sinienok@dev-pro.net>
2018-11-07 01:20:07 +0200

    Loops #2 done + task + launсh. Minor fixe for Loops #1

--- loops-1.1.cpp
+++ loops-1.1.cpp
@@ -1,29 +1,35 @@
 #include <cmath>
 #include <stdio.h>
 #include <iostream>
-
 using namespace std;
 
+// Вычисляем Z=|x| + x^3
 double calculateZ(double x) {
   return abs(x) + pow(x,3);
 } 
 
+// Вычисляем Y
 double calculateY(double x) {
   return calculateZ(x) * ((pow(sin(2+x), 2)/(2 + x)));
 }
 
 int main() {
-  
-
+  // Задаем края интервала
   int xmin = 1, xmax = 4; 
+
+  // Задаем шаг вычисления
   double hx = 0.5;
+
   cout << "Vhodnie dannue:" << endl;
-  cout << "        x=> [1,4]" << endl;
-  cout << "        Hx = 0.5 " << endl;
+  cout << "        x => [" << xmin << ", " << xmax << "]" << endl;
+  cout << "        Hx = " << hx << endl;
   cout << endl;
 
   cout << "Rezultat: " << endl;
+
+  //Текущее значение Х
   double currentX = xmin;
+  // Количество итераций цикла, в рассчетах не участвует, только для отображения
   int iterationsCount = 0;
   while(currentX <= xmax) {
     cout << "        Pri X=" << currentX << ", Y=" << calculateY(currentX) << endl;
---
+++ loops-1.2.cpp
@@ -0,0 +1,77 @@
+#include <cmath>
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+// Вычисляем значение Y на интервале,
+// где x < b
+double calculateYwhenXlessThanB (double a, double b, double x) {
+  return sin(abs(a * x + pow(b, a)));
+} 
+
+// Вычисляем значение Y на интервале,
+// где x >= b
+double calculateYwhenXmoreOrEqualToB (double a, double b, double x) {
+  return cos(abs(a * x - pow(b, a)));
+}
+
+// Вычисляем Y
+double calculateY (double a, double b, double x) {
+  // Если x < b, то вычисляем значение Y на интервале,
+  // где x < b
+  if (x < b) return calculateYwhenXlessThanB(a, b, x);
+
+  // Если x >= b, то вычисляем значение Y на интервале,
+  // где x >= b
+  if (x >= b) return calculateYwhenXmoreOrEqualToB(a, b, x);
+}
+
+int main() {
+  // Задаем края интервала для Х
+  double xmin = 0.5, xmax = 3.1; 
+
+  // Задаем края интервала для a
+  double amin = -0.1, amax = -1.4;
+
+  // Задаем шаг X
+  double hx = 0.21;
+
+  // Задаем шаг a
+  double ha = 0.1;
+
+  // Задаем значение b
+  double b = 1.5;
+
+  cout << "Vhodnie dannue:" << endl;
+  cout << "        x => [" << xmin << ", " << xmax << "]" << endl;
+  cout << "        Hx = " << hx << endl;
+  cout << "        a => [" << amin << ", " << amax << "]" << endl;
+  cout << "        Ha = " << ha << endl;
+  cout << "        b = " << b << endl;
+  cout << endl;
+
+  cout << "Rezultat: " << endl;
+
+  //Текущее значение Х
+  double currentX = xmin;
+  double currentA = amin;
+
+  // Количество итераций цикла, в рассчетах не участвует, только для отображения
+  int iterationsCount = 0;
+
+  while(currentX <= xmax || currentA >= amax) {
+    cout << "        Pri x = " << currentX << ", a = " << currentA << " and b = " << b << ", Y = " << calculateY(currentA, b, currentX) << endl;
+
+    // a и x меняются одновременно
+    currentX += hx;
+    currentA += -ha;
+    
+    iterationsCount++;
+  }
+
+  cout << endl;
+  cout << "Gotovo! Koli4estvo proidennux iteracii: " << iterationsCount << endl;
+  cout << "                            Teku6ee X = " << currentX << endl;
+  cout << "                            Teku6ee A = " << currentA << endl;
+  return 0;
+}
