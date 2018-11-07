c978b3b90a03789ac04798d5bced3adbd569cae3
Sergii Sinienok <sergii.sinienok@dev-pro.net>
2018-11-07 20:37:26 +0200

    Loops #4 done

--- loops-1.4.cpp
+++ loops-1.4.cpp
@@ -5,40 +5,42 @@
 using namespace std;
 
 // Вычисляем значение Y на интервале,
-// где x < b
-double calculateYwhenXlessThanB (double a, double b, double x) {
-  return sin(abs(a * x + pow(b, a)));
+// где x > 2
+float calculateYwhenXmoreThan2 (float a, float x) {
+  return cos(a * x + 2);
 } 
 
 // Вычисляем значение Y на интервале,
-// где x >= b
-double calculateYwhenXmoreOrEqualToB (double a, double b, double x) {
-  return cos(abs(a * x - pow(b, a)));
+// где x <= 2
+float calculateYwhenXlessOrEqualTo2 (float a, float x) {
+  return tan(abs(x - 2 * a));
 }
 
 // Вычисляем Y
-double calculateY (double a, double b, double x) {
-  // Если x < b, то вычисляем значение Y на интервале,
-  // где x < b
-  if (x < b) return calculateYwhenXlessThanB(a, b, x);
-
-  // Если x >= b, то вычисляем значение Y на интервале,
-  // где x >= b
-  if (x >= b) return calculateYwhenXmoreOrEqualToB(a, b, x);
+float calculateY (float a, float x) {
+  // Если x <= 2, то вычисляем значение Y на интервале,
+  // где x <= 2
+  if (x <= 2) {
+    calculateYwhenXlessOrEqualTo2(a, x);
+  } 
+  // В противном случае x > 2, вычисляем значение Y на интервале,
+  // где x > 2
+  return calculateYwhenXmoreThan2(a, x);
+  
 }
 
 int main() {
   // Задаем края интервала для Х
-  double xmin = 0.5, xmax = 3.1; 
+  float xmin = 0.5, xmax = 3.1; 
 
   // Задаем минимальное значение для a
-  double amin = 0.1;
+  float amin = 0.1;
 
   // Задаем шаг X
-  double hx = 0.2;
+  float hx = 0.2;
 
   // Задаем шаг a
-  double ha = 0.3;
+  float ha = 0.3;
 
   cout << "Vhodnie dannue:" << endl;
   cout << "        x in [" << xmin << ", " << xmax << "]" << endl;
@@ -50,18 +52,18 @@ int main() {
   cout << "Rezultat: " << endl;
 
   //Текущее значение Х
-  double currentX = xmin;
-  double currentA = amin;
+  float currentX = xmin;
+  float currentA = amin;
 
   // Количество итераций цикла, в рассчетах не участвует, только для отображения
   int iterationsCount = 0;
 
-  while(currentX <= xmax || currentA >= amax) {
-    cout << "        Pri x = " << currentX << ", a = " << currentA << " and b = " << b << ", Y = " << calculateY(currentA, b, currentX) << endl;
+  while(currentX >= xmin && currentX <= xmax && currentA >=amin) {
+    cout << "        Pri x = " << currentX << ", a = " << currentA << ", Y = " << calculateY(currentA, currentX) << endl;
 
     // a и x меняются одновременно
     currentX += hx;
-    currentA += -ha;
+    currentA += ha;
 
     iterationsCount++;
   }
